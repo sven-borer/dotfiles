ad1c310cb9df689b21943ada3dd598296b974f4e
Sven Borer <borer.sven@gmail.com>
2020-06-15 20:04:26 +0200

    tied up repo

--- scripts/.scripts/check-battery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BATTINFO=$(acpi -b)
-DISCHARGING=$(echo "$BATTINFO" | grep Discharging)
-BATTERYLEVEL=$(echo "$BATTINFO" | cut -d " " -f 4 | sed 's/%,//')
-WARNING=20
-TIMEOUT=5000
-[[ $DISCHARGING && $BATTERYLEVEL -lt $WARNING ]] && DISPLAY=:0 /usr/bin/notify-send -t $TIMEOUT -u normal "BATTERY LOW" "$BATTINFO" || exit 0
