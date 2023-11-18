e2f5511b4834006a2403f69c01c224687f55fb3f
svenborer <borer.sven@gmail.com>
2023-11-18 19:33:07 +0100

    New locker script.

--- scripts/.scripts/battery.sh
+++ scripts/.scripts/battery.sh
@@ -3,7 +3,7 @@ acpi -b | grep -v unavailable | awk -F'[,:%]' '{print $2, $3}' | {
     read -r status capacity
 
     if [ "$status" = Discharging -a "$capacity" -lt 20 ]; then
-        DISPLAY=:0 dunstify -h string:x-dunst-stack-tag:battery -u critical " Low battery ($capacity%)!"
+        DISPLAY=:0 dunstify -h string:x-dunst-stack-tag:battery -u critical "LOW BATTERY ($capacity%)!"
     fi
 
     if [ "$status" = Discharging -a "$capacity" -lt 10 ]; then
---
+++ scripts/.scripts/lock.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+swaylock --screenshots --indicator --indicator-radius 100 --indicator-thickness 7 --effect-vignette 0.5:0.5 --ring-color 303030 --key-hl-color 880033 --line-color 00000000 --inside-color 111111 --separator-color 00000000 --fade-in 1 --effect-blur 6x10 --effect-greyscale --font "Source Code Pro"
