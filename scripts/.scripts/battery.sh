4368c3a48a54195ae9897e52d71f8388500b3480
svenborer <borer.sven@gmail.com>
2023-11-22 21:06:01 +0100

    Format strings.

--- scripts/.scripts/battery.sh
+++ scripts/.scripts/battery.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 acpi -b | grep -v unavailable | awk -F'[,:%]' '{print $2, $3}' | {
     read -r status capacity
-
     if [ "$status" = Discharging -a "$capacity" -lt 20 ]; then
-        DISPLAY=:0 dunstify -h string:x-dunst-stack-tag:battery -u critical "LOW BATTERY ($capacity%)!"
+        DISPLAY=:0 dunstify -h string:x-dunst-stack-tag:battery -u critical "BATTERY LOW > $capacity"
     fi
-
     if [ "$status" = Discharging -a "$capacity" -lt 10 ]; then
         mpg123 $HOME/.scripts/low_battery.mp3
     fi
--- scripts/.scripts/lock.sh
+++ scripts/.scripts/lock.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-swaylock --screenshots --indicator --indicator-radius 100 --indicator-thickness 7 --effect-vignette 0.5:0.5 --ring-color 303030 --key-hl-color 880033 --line-color 00000000 --inside-color 111111 --separator-color 00000000 --fade-in 1 --effect-blur 6x10 --effect-greyscale --font "Source Code Pro"
+swaylock --screenshots --indicator \
+    --indicator-radius 100 \
+    --indicator-thickness 7 \
+    --effect-vignette 0.5:0.5 \
+    --ring-color 0f111a \
+    --key-hl-color 81A1C1 \
+    --line-color 0f111a \
+    --inside-color 0f111a \
+    --separator-color 0f111a \
+    --inside-ver-color 88C0D0 \
+    --line-ver-color 88C0D0 \
+    --ring-ver-color 88C0D0 \
+    --inside-wrong-color BF616A \
+    --line-wrong-color BF616A \
+    --ring-wrong-color BF616A \
+    --inside-clear-color EBCB8B \
+    --line-clear-color EBCB8B \
+    --ring-clear-color EBCB8B \
+    --bs-hl-color B48EAD \
+    --fade-in 0.3 \
+    --effect-blur 5x8 \
+    # --effect-greyscale \
+    --font "Source Code Pro"
